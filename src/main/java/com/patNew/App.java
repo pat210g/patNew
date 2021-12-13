50d6fbb94bfb7771c2c0089972f2f7b9f16c0b4d
Ubuntu <ubuntu@ip-172-31-81-226.ec2.internal>
2021-12-13 19:47:26 +0000

    new code change 2nd time

--- src/main/java/com/patNew/App.java
+++ src/main/java/com/patNew/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World, Welcome to Jenkins!" );
+        System.out.println( "Hello World, Welcome to Jenkins with CI/CD pipeline!" );
     }
 }
